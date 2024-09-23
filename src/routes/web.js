5d642f8da0f360ff516661667e85e214663fde74
Nguyen Dinh Khang <dinhkhang417sc@gmail.com>
2024-09-23 23:23:25 +0700

    up

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,17 +1,16 @@
 import express from "express";
 import homeController from "../controllers/HomeController";
 
-let router = express.Router();
+const router = express.Router();
 
-let initWebRoutes = (app) => {
-    //router.get("/", homeController.getHomePage);
+const initWebRoutes = (app) => {
+    //router.get('/', homeController.getHomePage);
     router.get("/", (req, res) => {
         return res.send("hello khang ND")    
     });
-
-    router.post('/webhook', homeController.postWebhook);
-    router.get('/webhook', homeController.getWebhook)
+    router.post('/webhook', homeController.postWebHook);
+    router.get('/webhook', homeController.getWebHook);
     return app.use('/', router);
 }
 
-module.exports = initWebRoutes;
+export default initWebRoutes;
