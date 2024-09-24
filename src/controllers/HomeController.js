b07d4183c56b231daa8d5a7efdf5fbbf1d4d82b8
Nguyen Dinh Khang <dinhkhang417sc@gmail.com>
2024-09-24 22:46:40 +0700

    up

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -9,37 +9,27 @@ const getHomePage = (req, res) => {
 };
 
 const postWebHook = (req, res) => {
-    let body = req.body;
+  let body = req.body;
 
-    // console.log(`\u{1F7EA} Received webhook:`);
-    // console.dir(body, { depth: null });
-    if (body.object === "page") {
-        body.entry.forEach(function(entry) {
+  if (body.object === 'page') {
+      body.entry.forEach(entry => {
+          let webhook_event = entry.messaging[0];
+          console.log(webhook_event);
 
-            // Gets the body of the webhook event
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-          
-          
-            // Get the sender PSID
-            let sender_psid = webhook_event.sender.id;
-            console.log('Sender PSID: ' + sender_psid);
-          
-            // Check if the event is a message or postback and
-            // pass the event to the appropriate handler function
-            if (webhook_event.message) {
-              handleMessage(sender_psid, webhook_event.message);        
-            } else if (webhook_event.postback) {
+          let sender_psid = webhook_event.sender.id;
+          console.log('Sender PSID: ' + sender_psid);
+
+          if (webhook_event.message) {
+              handleMessage(sender_psid, webhook_event.message);
+          } else if (webhook_event.postback) {
               handlePostback(sender_psid, webhook_event.postback);
-            }
-            
-          });
-  
-        // Return a '200 OK' response to all events
-        res.status(200).send('EVENT_RECEIVED');   
-    } else {
-        res.sendStatus(404);
-    }
+          }
+      });
+
+      res.status(200).send('EVENT_RECEIVED');
+  } else {
+      res.sendStatus(404);
+  }
 };
 
 const getWebHook = (req, res) => {
