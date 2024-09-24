a740b52c0d8dcc53bf27b9302a55e54af707a9ca
Nguyen Dinh Khang <dinhkhang417sc@gmail.com>
2024-09-24 23:03:35 +0700

    up

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from "dotenv";
 import viewEngine from './configs/viewEngine';
 import webRoutes from './routes/web';
-
+dotenv.config();
 let app = express();
 
 //config view engine
-viewEngine(app);
-webRoutes(app);
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+viewEngine(app);
+webRoutes(app);
 let port = process.env.PORT || 8080;
 
 app.listen(port, () => {
